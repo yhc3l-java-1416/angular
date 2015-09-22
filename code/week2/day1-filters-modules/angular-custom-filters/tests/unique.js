b05c815157a8aeaba53428bb8928388de86de836
Tom Blackmore <tablackmore@gmail.com>
2015-09-22 08:56:47 +0200

    Added day2 forms

--- code/week2/day1-filters-modules/angular-custom-filters/tests/unique.js
+++
@@ -1,8 +0,0 @@
-describe('unique filter', function () {
-	beforeEach(angular.mock.module('customFilters'));
-	it('returns an array with duplicates removed', function () {
-		angular.mock.inject(function (uniqueFilter) {
-			expect(uniqueFilter(['Tom', 'Fred', 'George', 'Harry', 'Tom'])).toEqual(['Tom', 'Fred', 'George', 'Harry']);
-		});
-	});
-});
